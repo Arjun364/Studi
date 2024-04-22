3ecadada7a5dda26c59e556c170e78c1a1ba4fcf
Arjun AV <avarjun360@gmail.com>
2024-04-23 00:41:18 +0530

    added viewport for userpage
    basic viewport structure is created.

--- web/src/Pages/UserPage.jsx
+++ web/src/Pages/UserPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import UserNav from '../component/UserHome/UserNav'
 import UserSidebar from '../component/UserHome/UserSidebar'
+import Viewport from '../component/UserHome/UserViewport/Viewport'
 
 const UserPage = () => {
   return (
     <>
     <UserNav/>
+    <div className="hero-section1">
     <UserSidebar/>
+    <Viewport/>
+    </div>
     </>
   )
 }
---
+++ web/src/component/UserHome/UserViewport/Viewport.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../userStyle.css'
+
+const viewport = () => {
+  return (
+    <>
+    <div className="viewport-container">
+        
+    </div>
+    </>
+  )
+}
+
+export default viewport
